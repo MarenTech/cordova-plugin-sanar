b5f1b0c8f9e24d336818cd3dac069e2927c6a027
abdulaziz <aziz@marensolutions>
2021-04-01 19:04:24 +0530

    added sanar demo call url

--- example/www/js/index.js
+++ example/www/js/index.js
@@ -30,7 +30,7 @@ function onDeviceReady() {
 }
 
 function startChat() {
-    let url = "" // Sanar Chat URL with SANAR_TOKEN
+    let url = "https://webprelive.litedev.com/bupa/c8NQD9ugZ72FV1dRdFcUypLzhjg7jS4Oymvur33xgo8ids%2BLcw5uQWBDDibftTVoSJtvCyAiBbawk77FiMG63lyx7MuEum3rJhJacNo8zBfkSpcEygTSg%2FpnQBzmftEDHvQpBQtNMBOC8AcLo96lflGoDXxDeYDYZfYBzR2YvlDQ5o7LBzl%2FjLQbh7SHie60c8mtfOOopGTnuVgLhKGG7KgCHPb69C3zdxEPY5DRCAolTOExTb1ZYDrFDkvTVQ%2Bn6nbyHDu%2FYI9wTbLbH3YWgAQzWllsg1bZ0v7joB9xcqQ%3D" // Sanar Chat URL with SANAR_TOKEN
     if(!url) {
         alert("Request Sanar Url");
         return
